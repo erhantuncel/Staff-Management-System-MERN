1e8476ff5c265ecb0c85f15d44daa6d705cf7513
Erhan TUNCEL <deverhn@gmail.com>
2025-02-14 23:00:12 +0300

    CHANGED: Hardcoded server port to environment variable.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Sever started at http://localhost:3000");
 });
 
