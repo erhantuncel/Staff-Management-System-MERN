658fd0589ffe9242825eb7054ffdfe818f33367a
Erhan TUNCEL <deverhn@gmail.com>
2025-03-28 14:32:31 +0300

    ADDED: Toast feature to frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+
 import UserDashboard from "./layouts/UserDashboard.jsx";
+import { Slide, ToastContainer } from "react-toastify";
 
 function App() {
-    return <UserDashboard />;
+    return (
+        <>
+            <UserDashboard />
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Slide}
+            />
+        </>
+    );
 }
 
 export default App;
