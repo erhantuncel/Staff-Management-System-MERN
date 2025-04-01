9bbb06ecb9bb157705287fa8c230802deeeee284
Erhan TUNCEL <deverhn@gmail.com>
2025-04-02 01:17:41 +0300

    ADDED: Staflist component and related modifications.

--- frontend/src/layouts/UserDashboard.jsx
+++ frontend/src/layouts/UserDashboard.jsx
@@ -9,7 +9,7 @@ const UserDashboard = () => {
     return (
         <div className="flex h-screen w-screen flex-col">
             <Header />
-            <div className="mb-auto">
+            <div className="m-10 mt-2 mb-auto lg:m-20 lg:mt-5 lg:mb-auto">
                 <Routes>
                     <Route path="/" element={<DepartmentList />} />
                     <Route path="/departments" element={<DepartmentList />} />
