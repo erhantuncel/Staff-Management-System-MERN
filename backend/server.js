e4d1077d8b36eaaef17ecbe4a5f388eb5d7ff9d7
Erhan TUNCEL <deverhn@gmail.com>
2025-05-13 23:36:47 +0300

    ADDED: Custom middleware for CORS handling.

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,13 @@ app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler);
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.listen(PORT, () => {
     connectDb();
     logger.info(`Sever started at http://localhost:${PORT}`);
