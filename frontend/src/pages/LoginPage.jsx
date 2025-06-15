23392538e1a90332f99fa7f9d68dcd1ec6d67a42
Erhan TUNCEL <deverhn@gmail.com>
2025-06-16 01:23:14 +0300

    ADDED: i18n translation to login and registration page.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,26 +1,52 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router";
 
 const LoginPage = () => {
+    const { t, i18n } = useTranslation();
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col">
                 <div className="text-center">
-                    <h1 className="mb-10 text-4xl font-bold">User Login</h1>
+                    <h1 className="mb-10 text-4xl font-bold">
+                        {t("LOGIN.hero.title")}
+                    </h1>
                 </div>
                 <div className="card bg-base-100 w-full min-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <label className="font-bold">User Name</label>
+                        <label className="font-bold">
+                            {t("LOGIN.input.label.userName")}
+                        </label>
                         <input type="text" className="input" />
-                        <label className="font-bold">Password</label>
+                        <label className="font-bold">
+                            {t("LOGIN.input.label.password")}
+                        </label>
                         <input type="password" className="input" />
-                        <button className="btn btn-neutral mt-4">Login</button>
+                        <button className="btn btn-neutral mt-4">
+                            {t("LOGIN.button.label")}
+                        </button>
                     </div>
                 </div>
-                <div className="mt-10 text-center text-sm">
-                    Don't have an account yet?{" "}
+                <div className="mt-10 flex justify-center gap-1 text-center text-sm">
+                    {t("LOGIN.not.exist.account.message")}
                     <Link className="link link-neutral" to="/register">
-                        Register
+                        {t("LOGIN.link.register")}
                     </Link>
+                </div>
+                <div className="mt-10 flex justify-center gap-1.5 text-center text-xs">
+                    <a
+                        className={i18n.language === "tr" ? "" : "link"}
+                        onClick={() => i18n.changeLanguage("tr")}
+                    >
+                        {t("TEMPLATE.FOOTER.link.turkish")}
+                    </a>
+                    |
+                    <a
+                        className={i18n.language === "en" ? "" : "link"}
+                        onClick={() => i18n.changeLanguage("en")}
+                    >
+                        {t("TEMPLATE.FOOTER.link.english")}
+                    </a>
                 </div>
             </div>
         </div>
